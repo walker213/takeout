e6e4029575b4da3c943ee6a0232024ea60ef68b2
walker213 <921254299@qq.com>
2020-03-06 22:34:05 +0800

    feat: add webpack entries of every page

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,34 @@
 const path = require("path");
+const fs = require("fs");
+const HtmlWebpack = require("html-webpack-plugin");
 const srcRoot = path.resolve(__dirname, "src");
-const devPath = path.resolve(__dirname, "dev");
-const pageDir = path.resolve(srcRoot, "page");
+const distPath = path.resolve(__dirname, "dist");
+const pageDir = path.resolve(srcRoot, "pages");
+const mainFile = "index.js";
+
+function getEntry() {
+  let entryMap = {};
+  fs.readdirSync(pageDir).forEach(pathname => {
+    let fullPathName = path.resolve(pageDir, pathname);
+    let stat = fs.statSync(fullPathName);
+    let fileName = path.resolve(fullPathName, mainFile);
+    if (stat.isDirectory() && fs.existsSync(fileName)) {
+      entryMap[pathname] = fileName;
+    }
+  });
+  return entryMap;
+}
+
+const entryMap = getEntry();
+console.log(entryMap);
 
 module.exports = {
-  entry: {},
-  output: {},
+  mode: "development",
+  entry: entryMap,
+  output: {
+    path: distPath,
+    filename: "[name].min.js"
+  },
   module: {
     rules: [
       { test: /\.css$/, use: ["style-loader", "css-loader"], include: srcRoot },
