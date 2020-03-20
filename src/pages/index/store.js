d169eac0c780901280e11a00998f76872179705d
walker213 <921254299@qq.com>
2020-03-21 00:04:39 +0800

    feat: config redux devtools

--- src/pages/index/store.js
+++ src/pages/index/store.js
@@ -1,8 +1,9 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 /* 检测rootreducer更改，然后热更新 */
 if (module.hot) {
