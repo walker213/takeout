7b3517ef76c525250bb11f6b65d3f7050da96e10
walker213 <921254299@qq.com>
2020-03-23 14:39:18 +0800

    feat: build ContentList frame

--- src/pages/index/Home/ContentList.jsx
+++ src/pages/index/Home/ContentList.jsx
@@ -21,6 +21,41 @@ class Category extends Component {
   //   this.props.dispatch(fetchCategoryData());
   // }
 
+  renderList() {
+    return (
+      <div className="shop-item scale-1px">
+        <div className="shop-img">
+          <span className="shop-tag">xx</span>
+          <img src="https://via.placeholder.com/86x65/FFFF00/808080" alt="" />
+        </div>
+        <div className="shop-info">
+          <h4 className="name">xx</h4>
+          <div className="count info-detail">
+            <div>
+              <span>star </span>
+              <span>月售</span>
+            </div>
+            <div>
+              <span className="time">xx分钟 | </span>
+              <span className="distance">xxm</span>
+            </div>
+          </div>
+          <div className="convey info-detail">
+            <div>
+              <span className="cost">起送 ￥30 | </span>
+              <span className="time">xx</span>
+            </div>
+            <div className="convey-tag">美团专送</div>
+          </div>
+          <div className="promotion">
+            <span>xx</span>
+            <span>xx</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="shop">
@@ -29,6 +64,7 @@ class Category extends Component {
           附近商家
           <i />
         </h3>
+        <div className="shop-list">{this.renderList()}</div>
       </div>
     );
   }
