99bb85108072ff724ee1de581e69614d299a269d
walker213 <921254299@qq.com>
2020-03-30 13:55:12 +0800

    fix: 修复设置rem以及图片加载引起的画面抖动

--- src/helper/rem.js
+++ src/helper/rem.js
@@ -1,8 +1,8 @@
-window.onload = () => {
+window.addEventListener("DOMContentLoaded", () => {
   const html = document.documentElement;
   function setRemUnit() {
     html.style.fontSize = html.clientWidth / 10 + "px";
   }
   setRemUnit();
   window.onresize = setRemUnit;
-};
+});
